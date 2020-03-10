853009cb6a3d5f6a0eb54c71169c93426081ff70
Vedika <vedika.j.kulkarni@gmail.com>
2020-03-10 23:01:15 +0530

    first

---
+++ app/src/main/java/com/example/cabsharing/ForgotAndChangePasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.cabsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotAndChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_and_change_password2);
+    }
+}
